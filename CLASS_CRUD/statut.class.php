7ae14773c8da1082948c03af91c2bca4d3e686c5
flo16r <flo@MacBook-Air-de-SEZER.local>
2022-02-15 16:41:05 +0100

    first comit

---
+++ CLASS_CRUD/statut.class.php
@@ -0,0 +1,81 @@
+<?php
+// CRUD STATUT
+// ETUD
+require_once __DIR__ . '../../CONNECT/database.php';
+
+class STATUT{
+	function get_1Statut($idStat){
+		global $db;
+
+        $query = "SELECT * FROM STATUT WHERE idStat = ?";
+        $result = $db->prepare($query);
+        $result->execute([$idStat]);
+		return($result->fetch());
+	}
+
+	function get_AllStatuts(){
+		global $db;
+
+		$query = 'SELECT * FROM STATUT;';
+        $result = $db->query($query);
+        $allStatuts = $result->fetchAll();
+		return($allStatuts);
+	}
+
+	function create($libStat){
+		global $db;
+
+		try {
+			$db->beginTransaction();
+
+			$query = 'INSERT INTO STATUT (libStat) VALUES (?);';
+            $request = $db->prepare($query);
+            $request->execute([$libStat]);
+			$db->commit();
+			$request->closeCursor();
+		}
+		catch (PDOException $e) {
+			$db->rollBack();
+			$request->closeCursor();
+			die('Erreur insert STATUT : ' . $e->getMessage());
+		}
+	}
+
+	function update($idStat, $libStat){
+		global $db;
+
+		try {
+			$db->beginTransaction();
+
+			$query = 'UPDATE STATUT SET libStat = ? WHERE idStat = ?;';
+            $request = $db->prepare($query);
+            $request->execute([$libStat, $idStat]);
+			$db->commit();
+			$request->closeCursor();
+		}
+		catch (PDOException $e) {
+			$db->rollBack();
+			$request->closeCursor();
+			die('Erreur update STATUT : ' . $e->getMessage());
+		}
+	}
+
+	function delete($idStat){
+		global $db;
+		
+		try {
+			$db->beginTransaction();
+
+			$query = 'DELETE FROM STATUT WHERE idStat = ?;';
+			$request = $db->prepare($query);
+            $request->execute([$idStat]);
+			$db->commit();
+			$request->closeCursor();
+		}
+		catch (PDOException $e) {
+			$db->rollBack();
+			$request->closeCursor();
+			die('Erreur delete STATUT : ' . $e->getMessage());
+		}
+	}
+}	// End of class
---
+++ back/statut/updateStatut.php
@@ -0,0 +1,120 @@
+<?php
+////////////////////////////////////////////////////////////
+//
+//  CRUD STATUT (PDO) - Modifié : 4 Juillet 2021
+//
+//  Script  : updateStatut.php  -  (ETUD)  BLOGART22
+//
+////////////////////////////////////////////////////////////
+
+// Mode DEV
+require_once __DIR__ . '/../../util/utilErrOn.php';
+
+// controle des saisies du formulaire
+require_once __DIR__ . '/../../util/ctrlSaisies.php';
+
+// Insertion classe Statut
+require_once __DIR__ . '/../../CLASS_CRUD/statut.class.php';
+
+// Instanciation de la classe Statut
+$statut = new STATUT();
+
+// Gestion des erreurs de saisie
+$erreur = false;
+
+// Gestion du $_SERVER["REQUEST_METHOD"] => En POST
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+//  controle des saisies du formulaire
+    $submit = isset($_POST['Submit']) ?? '';
+
+//    modification effective du statut
+    if ((isset($_POST['Sumit'])) AND ($submit === "Initialiser")) {
+        header("Location : ./updateStatut.php");
+    }
+
+    if (((isset($_POST['libStat'])) AND !empty($_POST['libStat']))
+        AND (!empty($_POST['Submit']) AND ($submit === "Valider"))) {
+//       saisie valide
+        $erreur = false;
+
+        $libStat = $_POST['libStat'];
+        $idStat = $_POST['idStat'];
+
+        $statut->update($idStat, $libStat);
+
+        header("Location: ./statut.php");
+    } else {
+//        saisies invalides
+        $erreur = true;
+        $errSaisies = "Erreur, la saisie est obligatoire !";
+    }
+}   // Fin if ($_SERVER["REQUEST_METHOD"] === "POST")
+// Init variables form
+include __DIR__ . '/initStatut.php';
+?>
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+    <meta charset="utf-8" />
+    <title>Admin - CRUD Statut</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <link href="../css/style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <h1>BLOGART22 Admin - CRUD Statut</h1>
+    <h2>Modification d'un statut</h2>
+<?php
+    $id = ($_GET['id'] ?? ($_POST['id'] ?? $idStat));
+    $statutEnCours = $statut->get_1Statut(['id']);
+    $libStat = $statutEnCours['libStat'];
+    $idStat = $statutEnCours['idStat'];
+
+?>
+    <form method="POST" action="<?= htmlspecialchars($_SERVER['PHP_SELF']); ?>" enctype="multipart/form-data" accept-charset="UTF-8">
+
+      <fieldset>
+        <legend class="legend1">Formulaire Statut...</legend>
+
+        <input type="hidden" id="id" name="id" value="<?= isset($_GET['id']) ?? ''; ?>" />
+
+        <div class="control-group">
+            <label class="control-label" for="libStat"><b>Nom :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></label>
+            <input type="text" name="libStat" id="libStat" size="80" maxlength="80" value="<?= $libStat; ?>" autofocus="autofocus" />
+        </div>
+
+        <div class="control-group">
+            <div class="error">
+<?php
+            if ($erreur) {
+                echo ($errSaisies);
+            }
+            else {
+                $errSaisies = "";
+                echo ($errSaisies);
+            }
+?>
+            </div>
+        </div>
+
+        <div class="control-group">
+            <div class="controls">
+                <br><br>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <input type="submit" value="Initialiser" style="cursor:pointer; padding:5px 20px; background-color:lightsteelblue; border:dotted 2px grey; border-radius:5px;" name="Submit" />
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <input type="submit" value="Valider" style="cursor:pointer; padding:5px 20px; background-color:lightsteelblue; border:dotted 2px grey; border-radius:5px;" name="Submit" />
+                <br>
+            </div>
+        </div>
+      </fieldset>
+    </form>
+<?php
+require_once __DIR__ . '/footerStatut.php';
+
+require_once __DIR__ . '/footer.php';
+?>
+</body>
+</html>
